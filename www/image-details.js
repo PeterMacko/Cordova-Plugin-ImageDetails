8ca42039eef62037b34f5eb3304d7ac10a9ec7e1
Macko <peter.macko@konicaminolta.sk>
2017-11-07 21:39:46 +0100

    Change js proxy

--- www/image-details.js
+++ www/image-details.js
@@ -1,7 +1,22 @@
-/*global cordova, module*/
+function ImageDetails() {
+}
 
-module.exports = {
-    resolution: function (fileName, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "ImageDetails", "resolution", [fileName]);
-    }
-};
+ImageDetails.prototype.resolution = function(filePath, sucess, failure) {
+	var args = {};
+	args.filePath = filePath;
+	cordova.exec(sucess, failure, "ImageDetails", "resolution", [args]);
+}
+
+cordova.addConstructor(function()  {
+	if(!window.plugins)
+	{
+		window.plugins = {};
+	}
+	
+   // shim to work in 1.5 and 1.6
+   if (!window.Cordova) {
+   	window.Cordova = cordova;
+   };
+   
+   window.plugins.ImageDetails = new ImageDetails();
+});
